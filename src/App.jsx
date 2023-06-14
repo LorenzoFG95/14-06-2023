de8636534116cd2cde52b72fc857750fd0b42ef6
LorenzoFG <lorenzofraterrigo@gmail.com>
2023-06-14 15:35:40 +0200

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Todo from './components/todo'
+import Title from './components/title'
+import './App.css'
+
+function App() {
+  return (
+  <div className="App">
+    <Title text='ToDooooooooo list'/>
+    <Todo/>
+  </div>
+  )
+}
+
+export default App
